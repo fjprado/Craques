a72daedb675b859c873a1006eb7266b7001a5ea4
Fernando José Prado <f.j.prado@hotmail.com>
2020-11-09 21:41:35 -0300

    Adicionar arquivos de projeto.

---
+++ Craques/Controllers/CraqueController.cs
@@ -0,0 +1,54 @@
+﻿using Craques.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Craques.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class CraqueController : ApiController
+    {
+        // GET: api/Craque
+        public IEnumerable<Craque> Get()
+        {
+            Craque craque = new Craque();
+            return craque.ListarCraque();
+        }
+
+        // GET: api/Craque/5
+        public Craque Get(int id)
+        {
+            Craque craque = new Craque();
+            return craque.ListarCraque().Where(item => item.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Craque
+        public List<Craque> Post(Craque craque)
+        {
+            Craque _craque = new Craque();
+            _craque.Inserir(craque);
+            return _craque.ListarCraque();
+        }
+
+        // PUT: api/Craque/5
+        public Craque Put(int id, [FromBody] Craque craque)
+        {
+            Craque _craque = new Craque();
+
+            return _craque.Atualizar(id, craque);
+        }
+
+        // DELETE: api/Craque/5
+        public string Delete(int id)
+        {
+            Craque _craque = new Craque();
+
+            _craque.Deletar(id);
+            return $"Craque ID {id} removido com sucesso";
+        }
+    }
+}
---
+++ Craques/Models/Craque.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Craques.Models
+{
+    public class Craque
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Posicao { get; set; }
+        public string Telefone { get; set; }
+        public int Cpf { get; set; }
+        public List<Craque> ListarCraque()
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data/Base.json");
+            var json = File.ReadAllText(caminhoArquivo);
+            var listaCraques = JsonConvert.DeserializeObject<List<Craque>>(json);
+            return listaCraques;
+        }
+
+        public bool RescreverArquivo(List<Craque> listaCraques)
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data/Base.json");
+            var json = JsonConvert.SerializeObject(listaCraques, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+
+            return true;
+        }
+
+
+        public Craque Inserir(Craque Craque)
+        {
+            var listaCraques = this.ListarCraque();
+
+            var maxId = listaCraques.Max(item => item.Id);
+            Craque.Id = maxId + 1;
+            listaCraques.Add(Craque);
+
+            RescreverArquivo(listaCraques);
+            return Craque;
+        }
+
+        public Craque Atualizar(int id, Craque Craque)
+        {
+            var listaCraques = this.ListarCraque();
+
+            var itemIndex = listaCraques.FindIndex(item => item.Id == id);
+            if (itemIndex >= 0)
+            {
+                Craque.Id = id;
+                listaCraques[itemIndex] = Craque;
+            }
+            else
+            {
+                return null;
+            }
+
+            RescreverArquivo(listaCraques);
+            return Craque;
+        }
+
+        public bool Deletar(int id)
+        {
+            var listaCraques = this.ListarCraque();
+
+            var itemIndex = listaCraques.FindIndex(item => item.Id == id);
+            if (itemIndex >= 0)
+            {
+                listaCraques.RemoveAt(itemIndex);
+            }
+            else
+            {
+                return false;
+            }
+
+            RescreverArquivo(listaCraques);
+            return true;
+        }
+    }
+}
