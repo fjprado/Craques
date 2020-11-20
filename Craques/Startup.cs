ac76068776826746cf07cba582a0ae473e73e4b2
Fernando J Prado <f.j.prado@hotmail.com>
2020-11-19 21:58:22 -0300

    Owin: - Added Owin to project.

---
+++ Craques/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly: OwinStartup(typeof(Craques.Startup))]
+
+namespace Craques
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseCors(CorsOptions.AllowAll);
+
+            app.UseWebApi(config);
+        }
+    }
+}
